0d78d97e2384c65b001d66d4efbc03ad8d3a42cf
SStefanie <1263616854@qq.com>
2024-11-15 21:57:46 +0800

    Update 浮现.js

--- Page/js/浮现.js
+++ Page/js/浮现.js
@@ -94,25 +94,25 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-document.addEventListener('DOMContentLoaded', function() {
-	var sb2button1 = document.getElementById('sb2button1');
-	var sb2button2 = document.getElementById('sb2button2');
-	var sb2button3 = document.getElementById('sb2button3');
-	var sb2button4 = document.getElementById('sb2button4');
-	sb2button1.addEventListener('click', function() {
-		window.location.href = "../医疗机器人/肢体反馈.html";
-	});
-	sb2button2.addEventListener('click', function() {
-		window.location.href = "../医疗机器人/电动站立.html";
-	});
-	sb2button3.addEventListener('click', function() {
-		window.location.href = "../医疗机器人/下肢穿戴.html";
-	});
-	sb2button4.addEventListener('click', function() {
-		window.location.href = "../医疗机器人/外骨骼.html";
-	});
+// document.addEventListener('DOMContentLoaded', function() {
+// 	var sb2button1 = document.getElementById('sb2button1');
+// 	var sb2button2 = document.getElementById('sb2button2');
+// 	var sb2button3 = document.getElementById('sb2button3');
+// 	var sb2button4 = document.getElementById('sb2button4');
+// 	sb2button1.addEventListener('click', function() {
+// 		window.location.href = "../医疗机器人/肢体反馈.html";
+// 	});
+// 	sb2button2.addEventListener('click', function() {
+// 		window.location.href = "../医疗机器人/电动站立.html";
+// 	});
+// 	sb2button3.addEventListener('click', function() {
+// 		window.location.href = "../医疗机器人/下肢穿戴.html";
+// 	});
+// 	sb2button4.addEventListener('click', function() {
+// 		window.location.href = "../医疗机器人/外骨骼.html";
+// 	});
 
-});
+// });
 const scrollToTopBtn = document.getElementById('scrollToTopBtn');
 
 
